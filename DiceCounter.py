04a87413c2f81386664fcfefb281771f6c1c88b1
its-dax <ddax.kn@gmail.com>
2021-12-04 16:14:42 +0100

    Detecting the number of dice

--- DiceCounter.py
+++ DiceCounter.py
@@ -1,68 +1,84 @@
 import cv2
-import numpy as np;
+import numpy as np
 
-
-BadAnswer = True
+impath = "Images/dices20.jpg"
 
 # Read image
-im = cv2.imread("Images/dices2.jpg", cv2.IMREAD_GRAYSCALE)
-imr = cv2.resize(im, (640, 480))
-(thresh, imbw) = cv2.threshold(im, 150, 255, cv2.THRESH_BINARY)
+im = cv2.imread(impath)
+img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+imr = cv2.resize(img, (640, 480))
+imb = cv2.GaussianBlur(imr, (11,11), 5)
+dilated = cv2.dilate(imb, (2,2), iterations = 15)
+canny = cv2.Canny(dilated, 70, 60, 130)
+
+dilated2 = cv2.dilate(canny, (1,1), iterations = 4)
+
 
 # Setup SimpleBlobDetector parameters.
 params = cv2.SimpleBlobDetector_Params()
-
-
-# while BadAnswer:
-#     color = input("Fehérek(w) vagy feketék(b) a pontok a dobókockán:  ")
-#     if color == "w":
-#         params.blobColor = 1;  
-#         BadAnswer = False  
-#     if color == "b":
-#         params.blobColor = 0;
-#         BadAnswer = False 
-
 params.filterByColor = True
-
-# Change thresholds
 params.minThreshold = 20;
 params.maxThreshold = 200;
- 
-# Filter by Area.
 params.filterByArea = True
-params.minArea = 50
-params.maxArea = 1500
- 
-# Filter by Circularity
+params.minArea = 77
+params.maxArea = 700
 params.filterByCircularity = True
-params.minCircularity = 0.77
+params.minCircularity = 0.75
 params.maxCircularity = 1.0
-
-# Filter by Convexity
 params.filterByConvexity = True
-params.minConvexity = 0.8
- 
-# Filter by Inertia
+params.minConvexity = 0.70
 params.filterByInertia = True
 params.minInertiaRatio = 0.01
 
-
 # Set up the detector with default parameters.
 detector = cv2.SimpleBlobDetector_create(params)
-
 # Detect blobs.
 keypoints = detector.detect(imr)
-
 #Number of keypoints detected
 value = len(keypoints)
 
 # Draw detected blobs as red circles.
-# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
 im_with_keypoints = cv2.drawKeypoints(imr, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
 #Put value on image 
-cv2.putText(im_with_keypoints,f"Value of the dices: {value} ", (10,30), cv2.FONT_HERSHEY_COMPLEX, 0.75, (0,0,255),2)
+cv2.putText(im_with_keypoints,f"Value: {value} ", (10,30), cv2.FONT_HERSHEY_COMPLEX, 0.75, (0,0,255),2)
+
+
+
+                                ##############################################
+                                ###############Counting dice##################
+                                
+
+
+
+# Setup SimpleBlobDetector parameters.
+params2 = cv2.SimpleBlobDetector_Params()
+params2.filterByColor = False
+params2.filterByArea = True
+params2.minArea = 5000
+params2.maxArea = 18000
+params2.filterByCircularity = False
+params2.minCircularity = 0
+params2.maxCircularity = 1.0
+params2.filterByConvexity = False
+params2.filterByInertia = False
+
+# Set up the detector with default parameters.
+detector2 = cv2.SimpleBlobDetector_create(params2)
+# Detect blobs.
+keypoints2 = detector2.detect(dilated)
+#Number of keypoints detected
+diceNum = len(keypoints2)
+
+# Draw detected blobs as red circles.
+im_with_keypoints = cv2.drawKeypoints(im_with_keypoints, keypoints2, np.array([]), (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+
+
+
+cv2.putText(im_with_keypoints,f"Number: {diceNum} ", (10,60), cv2.FONT_HERSHEY_COMPLEX, 0.75, (0,255,0),3)
 
 # Show keypoints
 cv2.imshow("Keypoints", im_with_keypoints)
+#cv2.imshow("Keypoints2", im_with_keypoints2)
+
 cv2.waitKey(0)
